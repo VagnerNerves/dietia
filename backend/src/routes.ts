6444b87f0183bac22b4cb2602fda6cca6bf39732
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-22 13:59:23 -0300

    feat: Initi projetc backend.

---
+++ backend/src/routes.ts
@@ -0,0 +1,17 @@
+import type {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyRequest,
+  FastifyReply,
+} from 'fastify'
+
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions
+) {
+  fastify.get('/teste', (request: FastifyRequest, reply: FastifyReply) => {
+    console.log('Rota Chamada!!!!')
+
+    reply.send({ ok: true })
+  })
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import dotenv from 'dotenv'
+import { routes } from './routes'
+
+const app = fastify()
+
+dotenv.config()
+
+app.setErrorHandler((error, request, reply) => {
+  reply.code(400).send({ message: error.message })
+})
+
+const start = async () => {
+  app.register(cors)
+
+  app.register(routes)
+
+  try {
+    await app.listen({ port: 333, host: '0.0.0.0' })
+
+    console.log('Servidor Run')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
